8ea36c713ff829e98d0bcc3f4f19b447c1900d08
shgoren <shahaf.goren@gmail.com>
2019-12-14 15:48:41 +0200

    fix

--- configuration.py
+++ configuration.py
@@ -1,31 +1,34 @@
-class Point():
-    '''
-    represent a 3d point in space
-    '''
 
-    def __init(self, x, y, z):
+
+class Point():
+    """
+    represent a 3d point in space
+    """
+
+    def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
 
 class Alignment():
-    '''
+    """
     represent a global 3d euler angles alignment
-    '''
+    """
 
-    def __init(self, gamma, beta, alpha):
+    def __init__(self, gamma, beta, alpha):
         self.gamma = gamma
         self.beta = beta
         self.alpha = alpha
 
+
 class Configuration():
     def __init__(self, point, alignment):
-        '''
+        """
         represents the configuration of a component.
         contains global position T=(x,y,z)
         contains global rotation Euler=(gamma,beta,alpha)
-        '''
+        """
 
         self.point = point
         self.alignment = alignment
--- parts_classes.py
+++ parts_classes.py
@@ -1,9 +1,10 @@
 from component import *
 
+
 class Gear(Component):
-    '''
+    """
     The gear center is at the origin and phase is the alpha from configuration
-    '''
+    """
 
     def __init__(self, configuration, radius):
         Component.__init__(self, configuration)
@@ -11,9 +12,9 @@ class Gear(Component):
 
 
 class Stick(Component):
-    '''
+    """
     the stick starts at the origin and it's direction is parallel to the local X axis
-    '''
+    """
 
     def __init__(self, configuration, length):
         Component.__init__(self, configuration)
