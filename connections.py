ae324d1dad55fd7e63409897faed04a1d0a2a0e3
MatanCohen1 <48020526+MatanCohen1@users.noreply.github.com>
2019-12-21 17:56:33 +0200

    added assemblies

--- connections.py
+++ connections.py
@@ -77,15 +77,17 @@ class Connection(ABC):
 
 class PinConnection(Connection):
 
-    def __init__(self, comp1, comp2, joint1, joint2):
+    def __init__(self, comp1, comp2, joint1, joint2, rotation1, rotation2):
         self.comp1 = comp1
         self.comp2 = comp2
         self.joint1 = joint1
         self.joint2 = joint2
+        self.rotation1 = rotation1
+        self.rotation2 = rotation2
 
     def get_constraint(self):
+        # self.comp1.get_global(self.joint1) - self.comp1.
         self.comp1.get_global(self.joint1) - self.comp2.get_global(self.joint2)
-
 
 class PhaseConnection(Connection):
 
