7b056478bed561f248a1a0843ae93049e49b831f
MatanCohen1 <48020526+MatanCohen1@users.noreply.github.com>
2020-01-10 17:39:10 +0200

    Merge remote-tracking branch 'origin/ofir/curves_distance_implementation' into matan/connections
    # Conflicts:
    #	assembly.py

--- generate_random_curve.py
+++ generate_random_curve.py
@@ -16,7 +16,7 @@ def generate_random_curve(number_of_curves , number_of_points=360 , curve_path =
         origin_assembly = create_assemblyA()
         assembly_curve = get_assembly_curve(origin_assembly, number_of_points=number_of_points)
         assembly_curve = normalize_curve(assembly_curve, origin_assembly.anchor)
-        json.dump(assembly_curve, open(path + rf"\curves\curve_{i}.j", "w"))
+        json.dump(assembly_curve, open(curve_path + rf"\curve_{i}.j", "w"))
 
 if __name__ == "__main__":
     generate_random_curve(*sys.argv[1:])
