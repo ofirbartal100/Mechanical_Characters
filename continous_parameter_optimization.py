fd4d1888a6d78f7816f8172e51ecd543fc40923e
Ofir Bartal <ofirbartal100@gmail.com>
2020-01-11 19:46:26 +0200

    created the main infrastructure, now left to implement the functions

--- continous_parameter_optimization.py
+++ continous_parameter_optimization.py
@@ -1,15 +1,15 @@
 from assembly import *
+import scipy.optimize
 
 
 class ContinousParameterOptimization:
 
-    def __init__(self,nearest_assembly,target_curve):
+    def __init__(self, nearest_assembly, target_curve):
         self.nearest_assembly = nearest_assembly
         self.target_curve = target_curve
 
         # make sure target_curve and assembly_curve has same number of points
         self._T = 360
-
 
     def _objective(self):
         def objective_function(p):
@@ -18,7 +18,7 @@ class ContinousParameterOptimization:
             # aligns the estimated curve
             assembly.align_with_curve(self.target_curve)
 
-            sum=0
+            sum = 0
             for t in range(self._T):
                 # get_curve_at calculates s_t as function of p at t
 
@@ -26,7 +26,7 @@ class ContinousParameterOptimization:
                     assembly.get_curve_at(t) - self.target_curve.get_curve_at(t),
                     assembly.get_curve_at(t) - self.target_curve.get_curve_at(t))
 
-            return .5*sum
+            return .5 * sum
 
         return objective_function
 
@@ -37,14 +37,28 @@ class ContinousParameterOptimization:
             # aligns the estimated curve
             assembly.align_with_curve(self.target_curve)
 
-            sum=0
+            sum = 0
             for t in range(self._T):
                 # get_curve_at calculates s_t as function of p at t
+                doX_dop = assembly.get_red_point_dop()  # in R^(3xP)
+                doX_doSt = assembly.get_red_point_doSt(t)
+                doC_dop = assembly.get_const_dop()
+                doC_doSt = assembly.get_const_doSt(t)
+                doC_doSt_inv = np.linalg.inv(doC_doSt.T @ doC_doSt) @ doC_doSt.T
+                doSt_dop = -doC_doSt_inv @ doC_dop
 
-                sum += np.dot(
-                    ,
-                    assembly.get_curve_at(t) - self.target_curve.get_curve_at(t))
+                #(doX_dop + doX_doSt @ doSt_dop) is in R^(3xP)
+                # assembly.get_curve_at(t) - self.target_curve.get_curve_at(t)) is in R^3
+                sum += (doX_dop + doX_doSt @ doSt_dop) @ (assembly.get_curve_at(t) - self.target_curve.get_curve_at(t))
 
-            return .5*sum
+            return sum
 
         return gradient_function
+
+    def optimize_parameters(self):
+        p = self.nearest_assembly.get_p()
+        ret = scipy.optimize.minimize(self._objective(), p, jac=self._jacobian(), method='L-BFGS-B')
+        if ret.success:
+            return ret['x']
+        else:
+            raise Exception("did not converge")
