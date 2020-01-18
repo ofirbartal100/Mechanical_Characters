8eb9668a709397b8492def79816292d27b85f2a6
MatanCohen1 <48020526+MatanCohen1@users.noreply.github.com>
2020-01-18 18:08:27 +0200

    Merge remote-tracking branch 'origin/ofir/try_again_constraints' into merged_matan generate random curve is working.
    # Conflicts:
    #	assembly.py
    #	assemlyA testing.py
    #	generate_random_curve.py

--- assemlyA testing.py
+++ assemlyA testing.py
@@ -1,20 +1,35 @@
 import json
 
 from assembly import *
-
+import dill
 # a = return_prototype()
 # print(is_vaild_assembleA(a))
 
-sample = AssemblyA_Sampler()
+# sample = AssemblyA_Sampler()
+# sample.create_assemblyA_database(10,num_of_samples_around=10, debug_mode=True)
+# database, curve_databas  = sample.get_database(), sample.get_curve_database()
+# print(len(database))
+# print(len(curve_databas))
+# sample.save(r"C:\Users\A\Desktop\temp")
+#
+# for i,ass in enumerate(database):
+#     ass.plot_assembly(plot_path =r"C:\Users\A\Desktop\temp" , image_number = i,save_images = True)
 
-sample.create_assemblyA_database(300)
-database, curve_databas  = sample.get_database(), sample.get_curve_database()
-print(len(database))
-print(len(curve_databas))
+input_file = open(r"C:\Users\A\Desktop\temp\sampler", 'rb')
+sample = dill.load(input_file)
 
-for i,ass in enumerate(database):
-    ass.plot_assembly(plot_path =r"C:\Users\A\Desktop\temp" , image_number = i,save_images = True)
+target = create_assemblyA()
+print(is_vaild_assembleA(target))
+target_curve = get_assembly_curve(target,number_of_points=72)
+db_closest_curve, all_dist = sample.get_closest_curve(target_curve, get_all_dis = True)
+for k in all_dist:
+    print(all_dist[k])
+print("-------")
+print(all_dist[db_closest_curve])
 
+
+# input_file = open(r"C:\Users\A\Desktop\temp\sampler", 'rb')
+# sample = dill.load(input_file)
 
 # database, curve_databas = create_assemblyA_database(10)
 #
